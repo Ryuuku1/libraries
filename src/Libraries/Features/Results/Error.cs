6d87e35068c24d4f39a3e51c6758eafeedf6d4bb
Diogo Silva <dsi@contentguru.com>
2024-05-03 16:35:49 +0100

    Add result feature

---
+++ src/Libraries/Features/Results/Error.cs
@@ -0,0 +1,17 @@
+﻿namespace Libraries.Features.Results;
+
+/// <summary>
+/// Represents an error with a message.
+/// </summary>
+public record Error(string Message, string Title)
+{
+    /// <summary>
+    /// Represents an empty error.
+    /// </summary>
+    public static readonly Error None = new(string.Empty, string.Empty);
+
+    /// <summary>
+    /// Represents an error indicating that a null value was provided.
+    /// </summary>
+    public static readonly Error NullValue = new("Null value was provided.", "Null value.");
+}
