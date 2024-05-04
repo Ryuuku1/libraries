3765cdc1cc4d1d427cc849bd8e22b0c970898936
Diogo Silva <dsi@contentguru.com>
2024-05-04 02:35:46 +0100

    Add repositories

---
+++ src/Libraries/Features/Repositories/Queries/IGetEntity.cs
@@ -0,0 +1,23 @@
+﻿using Libraries.Features.Entities;
+
+namespace Libraries.Features.Repositories.Queries;
+
+/// <summary>
+/// Represents an interface for getting an entity by its ID.
+/// </summary>
+/// <typeparam name="T">The type of the entity.</typeparam>
+public interface IGetEntity<T>
+    where T : Entity
+{
+    /// <summary>
+    /// Gets an entity by its ID asynchronously.
+    /// </summary>
+    /// <param name="id">The ID of the entity.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The entity if found, otherwise null.</returns>
+    Task<T> GetByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default,
+        bool withTracking = false
+    );
+}
